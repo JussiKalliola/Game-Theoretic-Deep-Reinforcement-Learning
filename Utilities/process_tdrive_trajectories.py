30dd5a116f3e36c1e581b2348b38f5273e115fce
JussiKalliola <kalliolaj@gmail.com>
2025-01-08 12:37:35 +0200

    fixes

--- Utilities/process_tdrive_trajectories.py
+++ Utilities/process_tdrive_trajectories.py
@@ -22,10 +22,11 @@ def combine_tdrive_trajectories(dir_name: str) -> str:
         D.append(pd.read_csv(file_path,header=None,parse_dates = [1],\
                             names= ['vehicle_id', 'time', 'longitude', 'latitude']))
 
+    Data["order_number"] = 0
+    Data = Data['vehicle_id', 'order_number', 'time', 'longitude', 'latitude']
     Data=Data.append(D,ignore_index=True)
     Data.drop_duplicates(inplace=True)
     Data.dropna(inplace=True)
-    Data["order_number"] = 0
 
     print("Saving data to a path.")
     # Combined dataframe saved to root folder
@@ -36,11 +37,11 @@ if __name__ == "__main__":
     """Convert T-Drive trajectory dataset into suitable format."""
     trajectories_dir_name: str = 'CSV/T-Drive/logs/'
     out_file_name = combine_tdrive_trajectories(trajectories_dir_name)
-    longitude_min: float = 116.50000
+    longitude_min: float = 117.0000
     latitude_min: float = 39.00
     trajectories_time_start: str = '2008-02-02 16:00:00'
-    trajectories_time_end: str = '2008-02-02 17:00:00'
-    trajectories_out_file_name: str = 'CSV/T-Drive/trajectories_20080202_1600_1700'
+    trajectories_time_end: str = '2008-02-02 19:00:00'
+    trajectories_out_file_name: str = 'CSV/T-Drive/trajectories_20080202_1600_1900'
     edge_number: int = 9
     communication_range: float = 500
     
